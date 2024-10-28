f580b526d38f93a8df971cfdb62d7417020899d3
Aaron Gullickson <aarongullickson@gmail.com>
2024-10-28 12:19:27 -0700

    Add mixing summary to check_simulations.qmd

--- utils/functions.R
+++ utils/functions.R
@@ -1,5 +1,7 @@
 # functions.R
 
+# Demographic summary functions ---------------------------------------------
+
 # functions shared across scripts are placed here
 plot_pop_pyramid <- function(pop, date, age_width = 5) {
   dat_pyramid <- pop |>
@@ -59,4 +61,85 @@ calculate_lor <- function(marriages) {
               lor = log((n12 * n21) / (n11 * n22))) |>
     select(time_period, lor) |>
     filter(lor > -Inf)
+}
+
+# Genealogical functions --------------------------------------------------
+
+# create a dataset taht gives us pairing information of parents for each 
+# person in the population.
+get_parent_info <- function(pop) {
+  
+  # the zero ids for mom and dad in the data should  be NAs to clearly
+  # identify when we have hit the founder population
+  pop <- pop |>
+    mutate(mom = ifelse(mom == 0, NA, mom),
+          dad = ifelse(dad == 0, NA, dad))
+
+
+  # create a dataset that gives us mom and dad information for each current
+  # person, based on mom and dad
+  moms <- pop |>
+    filter(fem == 1) |>
+    select(pid, group) |>
+    rename(mom = pid, mom_group = group)
+
+  dads <- pop |>
+    filter(fem == 0) |>
+    select(pid, group) |>
+    rename(dad = pid, dad_group = group)
+                                
+  pop |>
+    left_join(moms) |>
+    left_join(dads) |>
+    select(pid, fem, group, dob, dod, mom, dad, mom_group, dad_group) |>
+    mutate(intermar = mom_group != dad_group)
+}
+
+# A function to grab all the next generation of parent info's from a single
+# parent info record
+get_next_ancestor_pairings <- function(record, parent_info) {
+  next_ancestors <- parent_info[c(record$mom, record$dad),]
+  # using the slightly more awkward coding above which depends on 
+  # actual position because the more elegant code below does not 
+  # correctly add people twice when they are duplicated in mom or dad
+  # vector due to inbreeding somewhere in the line. Might be a safer
+  # way to do this as I don't like depending on position.
+  #next_ancestors <- pairings |> 
+  #  filter(pid %in% record$mom | pid %in% record$dad)
+  return(next_ancestors)
+}
+
+# a function based on a pid value to collect all ancestors and calculate summary 
+# stats
+get_ancestry_summary <- function(id, parent_info) {
+  current_ancestors <- parent_info |> filter(pid == id)
+  gen <- 1
+  current_ancestors$gen <- gen
+  ancestors <- current_ancestors
+  while(nrow(current_ancestors) > 0) {
+    gen <- gen + 1
+    next_ancestors <- get_next_ancestor_pairings(current_ancestors, parent_info)
+    next_ancestors$gen <- gen
+    ancestors <- ancestors |> bind_rows(next_ancestors)
+    current_ancestors <- next_ancestors |>
+      filter(!is.na(mom))
+  }
+
+  orig_ancestors <- ancestors |>
+    filter(is.na(mom)) |>
+    mutate(ancestry_fraction = 1/(2^(gen-1)),
+           ancestry_group1 = ifelse(group == 1, ancestry_fraction, 0)) |>
+    select(pid, fem, group, gen, starts_with("ancestry_"))
+
+  ancestors_intermar <- ancestors |>
+    filter(intermar)
+
+  return(tibble(pid = id, 
+                ancestry_fraction = sum(orig_ancestors$ancestry_fraction),
+                ancestry_group1 = sum(orig_ancestors$ancestry_group1),
+                ancestry_group2 = 1 - ancestry_group1,
+                nearest_gen_locus = ifelse(nrow(ancestors_intermar) == 0, 
+                                           NA, 
+                                           min(ancestors_intermar$gen))))
+
 }
