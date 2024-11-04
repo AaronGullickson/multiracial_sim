612c4b9a0574414828a9a2563d6fee87f3d5fc34
Aaron Gullickson <aarongullickson@gmail.com>
2024-11-04 09:32:35 -0800

    Use googlesheet data to run simulations

--- simulation/run_simulations.R
+++ simulation/run_simulations.R
@@ -6,16 +6,23 @@ source(here("utils", "check_packages.R"))
 source(here("utils", "functions.R"))
 source(here("simulation", "simulation_functions.R"))
 
+# base folder for the sim data
 base_folder = here("data", "data_constructed", "sims")
 # check to see if base folder exists, and if not, create it
 if(!dir_exists(base_folder)) {
   dir_create(base_folder)
 }
 
+# output path for the reports
+output_path <- here("_products", "sim_diagnostics")
+if(!dir_exists(output_path)) {
+  dir_create(output_path)
+}
+
 
 seed <- 42
 
-fert_multiplier <- 1.09
+sheet_id <- "18jeYYzzQxIGWYdt7H9VWyo1T1yrUQt_jlydeE2f2uCs"
 
 # Create starter pop ------------------------------------------------------
 
@@ -23,99 +30,75 @@ fert_multiplier <- 1.09
 size_opop <-  30000
 
 # Create data.frame with 14 columns and nrows = size_opop
-presim_even.opop <- setNames(data.frame(matrix(data = 0, ncol = 14, nrow = size_opop)), 
+presim_opop <- setNames(data.frame(matrix(data = 0, ncol = 14, nrow = size_opop)), 
                         c("pid","fem","group","nev","dob","mom","pop",
                           "nesibm","nesibp","lborn","marid","mstat","dod","fmult"))
 
 # Add pid 1:sizeopop
-presim_even.opop$pid <- 1:size_opop
+presim_opop$pid <- 1:size_opop
 
 # Add sex randomly
-presim_even.opop$fem <- sample(0:1, nrow(presim_even.opop), replace = T)
+presim_opop$fem <- sample(0:1, nrow(presim_opop), replace = T)
 
 # Add random dates of birth (max age around 70)
-presim_even.opop$dob <- sample(360:1200, nrow(presim_even.opop), replace = T)
-
-# sample between two groups 
-presim_uneven.opop <- presim_even.opop
-
-presim_even.opop$group <- sample(1:2, nrow(presim_even.opop), replace = T,
-                            prob = c(0.5, 0.5))
-
-presim_uneven.opop$group <- sample(1:2, nrow(presim_uneven.opop), replace = T,
-                            prob = c(0.8, 0.2))
+presim_opop$dob <- sample(360:1200, nrow(presim_opop), replace = T)
 
 
-# Run baseline simulations ---------------------------------------------------
+# Run simulations from googlesheets ---------------------------------------
 
-run_simulation("even_hypo_baseline", 
-               presim_even.opop, 
-               segments = rep(10, 30),
-               endogamy = rep(0.999, 30),
-               inheritance = rep(0.95, 30),
-               fert_multiplier = fert_multiplier)
+sim_names <- googlesheets4::sheet_names(sheet_id)
 
-run_simulation("even_hyper_baseline",
-               presim_even.opop,
-               segments = rep(10, 30),
-               endogamy = rep(0.999, 30),
-               inheritance = rep(0.05, 30),
-               fert_multiplier = fert_multiplier)
-
-run_simulation("even_random_baseline",
-               presim_even.opop,
-               segments = rep(10, 30),
-               endogamy = rep(0.999, 30),
-               inheritance = rep(0.5, 30),
-               fert_multiplier = fert_multiplier)
-
-run_simulation("uneven_hypo_baseline",
-               presim_uneven.opop,
-               segments = rep(10, 30),
-               endogamy = rep(0.999, 30),
-               inheritance = rep(0.95, 30),
-               fert_multiplier = fert_multiplier)
-
-run_simulation("uneven_hyper_baseline",
-               presim_uneven.opop,
-               segments = rep(10, 30),
-               endogamy = rep(0.999, 30),
-               inheritance = rep(0.05, 30),
-               fert_multiplier = fert_multiplier)
-
-run_simulation("uneven_random_baseline",
-               presim_uneven.opop,
-               segments = rep(10, 30),
-               endogamy = rep(0.999, 30),
-               inheritance = rep(0.5, 30),
-               fert_multiplier = fert_multiplier)
-
-# Run full simulations ---------------------------------------------------
-
-# base off the uneven hypo
-pop_uneven_hypo <- read_csv(here(base_folder, "uneven_hypo_baseline",
-                                 "final_pop.csv"))
-mar_uneven_hypo <- read_csv(here(base_folder, "uneven_hypo_baseline",
-                                 "final_mar.csv"))
-ancestry_uneven_hypo <- read_csv(here(base_folder, "uneven_hypo_baseline", 
-                                      "ancestry.csv"))
-
-# add 100 years of increasing exogamy but no change in hypodescent
-run_simulation("uneven_hypo_increase",
-               pop_uneven_hypo,
-               segments = c(rep(5, 20)),
-               endogamy = seq(from = 0.989, by = -0.005, length.out = 20),
-               inheritance = rep(0.95, 20),
-               mar = mar_uneven_hypo,
-               ancestry = ancestry_uneven_hypo,
-               fert_multiplier = fert_multiplier)
-
-# same as above, but change to random
-run_simulation("uneven_hypo_increase_change",
-               pop_uneven_hypo,
-               segments = rep(5, 20),
-               endogamy = seq(from = 0.989, by = -0.005, length.out = 20),
-               inheritance = seq(from = 0.95, to = 0.5, length.out = 21)[-1],
-               mar = mar_uneven_hypo,
-               ancestry = ancestry_uneven_hypo,
-               fert_multiplier = fert_multiplier)
+for(sim_name in sim_names) {
+  
+  # read data from googlesheets
+  sim_start <- googlesheets4::range_read(sheet_id, 
+                                         range = paste(sim_name, "A2:C3", 
+                                                       sep="!"))
+  
+  sim_segments <- googlesheets4::range_read(sheet_id, 
+                                            range = paste(sim_name, "A6:C1000", 
+                                                          sep="!")) |>
+    filter(!is.na(segment_length))
+  
+  # get starting pop stuff
+  if(is.na(sim_start$starting_sim)) {
+    pop_start <- presim_opop |>
+      mutate(group = sample(1:2, nrow(presim_opop), replace = T, 
+                            prob = c(sim_start$group1_prop, 
+                                     1 - sim_start$group1_prop)))
+    mar_start <- NULL
+    ancestry_start <- NULL
+  } else {
+    pop_start <- read_csv(here(base_folder, sim_start$starting_sim, 
+                               "final_pop.csv"))
+    mar_start <- read_csv(here(base_folder, sim_start$starting_sim,
+                               "final_mar.csv"))
+    ancestry_start <- read_csv(here(base_folder, sim_start$starting_sim, 
+                                    "ancestry.csv"))
+  }
+  
+  # run the simulation
+  run_simulation(sim_name, 
+                 pop_start = pop_start,
+                 mar = mar_start,
+                 ancestry = ancestry_start,
+                 segments = sim_segments$segment_length,
+                 endogamy = sim_segments$endogamy,
+                 inheritance = sim_segments$inheritance)
+  
+  # now create the report
+  # annoyingly, I have to set the working directory here to get it to work. 
+  # setting execute_dir argument does not work.
+  setwd(here("simulation"))
+  report_name <- paste("diagnostics_", sim_name, ".html", sep="")
+  quarto_render(input = here("simulation", "check_simulation.qmd"), 
+                output_format = "html",
+                output_file = report_name,
+                execute_params = list(sim = sim_name))
+  # annoyingly, it will not put them where they belong, so lets move the report
+  # manually over to products
+  file_move(here("simulation", report_name), output_path)
+  # set working directory back
+  setwd(here(""))
+  
+}
