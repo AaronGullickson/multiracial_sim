eee6bdd4777c6fdf8768caa511b2728dcf3c66d5
Aaron Gullickson <aarongullickson@gmail.com>
2024-11-20 11:41:28 -0800

    Add cumulative probability plot as accordion for shiny app

--- simulation/inheritance_shiny/app.R
+++ simulation/inheritance_shiny/app.R
@@ -39,14 +39,22 @@ ui <- page_sidebar(
                 max = 0,
                 value = -3)
   ),
-  
-  plotOutput("distPlot")
+  accordion(
+    accordion_panel(
+      "Probability",
+      plotOutput("probPlot")
+    ),
+    accordion_panel(
+      "Cumulative Probability",
+      plotOutput("cumProbPlot")
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
+    output$probPlot <- renderPlot({
       
       tibble(
         prop_group1 = seq(from = 0, to = 1, by = .05),
@@ -79,6 +87,41 @@ server <- function(input, output) {
         #hist(x, breaks = bins, col = 'darkgray', border = 'white',
         #     xlab = 'Waiting time to next eruption (in mins)',
         #     main = 'Histogram of waiting times')
+    })
+    
+    output$cumProbPlot <- renderPlot({
+      
+      tibble(
+        prop_group1 = seq(from = 0, to = 1, by = .05),
+        logodds1 = input$group1_intercept+input$group1_slope*prop_group1,
+        logodds2 = input$group2_intercept+input$group2_slope*prop_group1,
+        logodds3 = 0,
+        group1 = round(exp(logodds1)/(exp(logodds1)+exp(logodds2)+exp(logodds3)), 4),
+        group2 = round(exp(logodds2)/(exp(logodds1)+exp(logodds2)+exp(logodds3)), 4),
+        group3 = round(exp(logodds3)/(exp(logodds1)+exp(logodds2)+exp(logodds3)), 4)
+      ) |>
+        select(prop_group1, starts_with("group")) |>
+        pivot_longer(cols = starts_with("group"), 
+                     names_to = "group", values_to = "prob") |>
+        mutate(group = factor(group, 
+                              levels = c("group1", "group2", "group3"),
+                              labels =c ("Group 1", "Group 2", "Mixed Group"))) |>
+        ggplot(aes(x = prop_group1, y = prob, fill = group, group = group))+
+        geom_area()+
+        scale_y_continuous(labels = scales::percent)+
+        labs(x = "proportion of ancestry from group 1",
+             y = "probability of being classified with given group")+
+        theme_bw()
+      
+      
+      # generate bins based on input$bins from ui.R
+      #x    <- faithful[, 2]
+      #bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      
+      # draw the histogram with the specified number of bins
+      #hist(x, breaks = bins, col = 'darkgray', border = 'white',
+      #     xlab = 'Waiting time to next eruption (in mins)',
+      #     main = 'Histogram of waiting times')
     })
 }
 
