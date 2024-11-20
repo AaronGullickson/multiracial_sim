83bfd95c9f37703ea4159aca67814fa4a831b3c9
Aaron Gullickson <aarongullickson@gmail.com>
2024-11-20 12:18:19 -0800

    Update comments in shiny app

--- simulation/inheritance_shiny/app.R
+++ simulation/inheritance_shiny/app.R
@@ -1,11 +1,6 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# Shiny app to visualize how group inheritance parameters lead to different
+# scenarios
 
 library(shiny)
 library(ggplot2)
@@ -120,16 +115,6 @@ server <- function(input, output) {
         labs(x = "proportion of ancestry from group 1",
              y = "probability of being classified with given group")+
         theme_bw()
-      
-      
-      # generate bins based on input$bins from ui.R
-      #x    <- faithful[, 2]
-      #bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      #hist(x, breaks = bins, col = 'darkgray', border = 'white',
-      #     xlab = 'Waiting time to next eruption (in mins)',
-      #     main = 'Histogram of waiting times')
     })
 }
 
