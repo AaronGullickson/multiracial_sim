17536a763ad94dbe30fee7f4893420da023f03e1
Aaron Gullickson <aarongullickson@gmail.com>
2025-07-08 14:28:51 -0700

    Rename debugging to run_simulations.R

--- simulation/run_simulations.R
+++ simulation/run_simulations.R
@@ -11,16 +11,18 @@ if (!nzchar(Sys.getenv("QUARTO_PROJECT_RENDER_ALL"))) {
   #quit()
 }
 
-# Load libraries and basics ----------------------------------------------
 
 library(here)
 source(here("utils", "check_packages.R"))
 source(here("utils", "functions.R"))
 source(here("simulation", "simulation_functions.R"))
 
+
+# Set up base information -------------------------------------------------
+
 # base folder for the sim data
 base_folder = here("data", "data_constructed", "sims")
-# check to see if base folder exists, and if not, create it
+# remove old base folder and create new one
 if(dir_exists(base_folder)) {
   dir_delete(base_folder)
 }
@@ -36,16 +38,14 @@ if(!dir_exists(output_path)) {
 seed <- sample(1:100, 1)
 set.seed(seed)
 
-# sheet id to read from on google sheets
+# set up google sheet for reading
 sheet_id <- "1ad-fJUCjRy_zslI2MMce8UaolepG536-IZr1yNNIuZ8"
-
-# de-authorize googlesheets4 so it won't ask about authorization
 googlesheets4::gs4_deauth()
 
 # Create starter pop ------------------------------------------------------
 
 # Set size of initial population
-size_opop <- 1000 #50000
+size_opop <-  1000
 
 # Create data.frame with 14 columns and nrows = size_opop
 presim_opop <- setNames(data.frame(matrix(data = 0, ncol = 14, nrow = size_opop)), 
@@ -64,109 +64,76 @@ presim_opop$fem <- sample(0:1, nrow(presim_opop), replace = TRUE)
 presim_opop$dob <- sample(360:1200, nrow(presim_opop), replace = TRUE)
 
 
-# Test simulations --------------------------------------------------------
-
-# sim_name <- "test"
-# pop_start <- presim_opop |>
-#   mutate(group = sample(1:2, nrow(presim_opop), replace = T,
-#                         prob = c(0.8, 0.2)))
-# segment_df <- tribble(
-#   ~segment_length, ~lodds12, ~lodds13, ~lodds23, ~inherit_g1_intercept, ~inherit_g1_slope, ~inherit_g2_intercept, ~inherit_g2_slope,
-#   300, -5, NA, NA, 3, 3, 10, -3
-# )
-# fert_multiplier <- 1.09
-# mar <- NULL
-# ancestry <- NULL
-# 
-# run_simulation("test",
-#                pop_start,
-#                segment_df,
-#                fert_multiplier = fert_multiplier)
-
-# Run simulations from googlesheets ---------------------------------------
+# Run the sims ------------------------------------------------------------
 
 sim_names <- googlesheets4::sheet_names(sheet_id)
 
 for(sim_name in sim_names) {
-  
+
   if(str_detect(sim_name, "IGNORE$")) {
     next
   }
-  
-  # read data from googlesheets
+
+  # get sim parameters
   sim_param <- get_sim_parameters(sim_name, sheet_id)
+  segment_df <- sim_param$segments
+  fert_multiplier <- sim_param$start$fert_multiplier
   
-  # annoying but something about the future processing is not clearing properly
-  # between runs, so I need to clear it manually here.
+  # get starting data
+  if(is.na(sim_param$start$starting_sim)) {
+    pop_start <- presim_opop |>
+      mutate(group = sample(1:2, nrow(presim_opop), replace = T, 
+                            prob = c(sim_param$start$group1_prop, 
+                                     1 - sim_param$start$group1_prop)))
+    mar <- NULL
+    ancestry <- NULL
+    segment_df$segment_length <- 10
+  } else {
+    pop_start <- read_csv(here(base_folder, 
+                               sim_param$start$starting_sim, 
+                               "final_pop.csv"))
+    mar <- read_csv(here(base_folder, 
+                         sim_param$start$starting_sim,
+                         "final_mar.csv"))
+    ancestry <- read_csv(here(base_folder, 
+                              sim_param$start$starting_sim, 
+                              "ancestry.csv"))
+    segment_df$segment_length <- 2
+  }
+
+
+  # reset future so we don't get shenanigans
   future::plan(sequential)
   gc()
   
-  tryCatch(
-    expr = {
-      # get starting pop stuff
-      if(is.na(sim_param$start$starting_sim)) {
-        pop_start <- presim_opop |>
-          mutate(group = sample(1:2, nrow(presim_opop), replace = T, 
-                                prob = c(sim_param$start$group1_prop, 
-                                         1 - sim_param$start$group1_prop)))
-        mar_start <- NULL
-        ancestry_start <- NULL
-      } else {
-        pop_start <- read_csv(here(base_folder, 
-                                   sim_param$start$starting_sim, 
-                                   "final_pop.csv"))
-        mar_start <- read_csv(here(base_folder, 
-                                   sim_param$start$starting_sim,
-                                   "final_mar.csv"))
-        ancestry_start <- read_csv(here(base_folder, 
-                                        sim_param$start$starting_sim, 
-                                        "ancestry.csv"))
-      }
-      
-      # just for testing
-      sim_param$segments$segment_length <- 10
-      
-      # run the simulation
-      run_simulation(sim_name, 
-                     pop_start = pop_start,
-                     segment_df = sim_param$segments,
-                     mar = mar_start,
-                     ancestry = ancestry_start,
-                     fert_multiplier = sim_param$start$fert_multiplier,
-                     seed = seed)
-      
-      # now create the report
-      # annoyingly, I have to set the working directory here to get it to work. 
-      # setting execute_dir argument does not work.
-      setwd(here("simulation"))
-      report_name <- paste("diagnostics_", sim_name, ".html", sep="")
-      quarto_render(input = here("simulation", "check_simulation.qmd"), 
-                    output_format = "html",
-                    output_file = report_name,
-                    execute_params = list(sim = sim_name, sheet_id = sheet_id))
-      # annoyingly, it will not put them where they belong, so lets move the report
-      # manually over to products
-      file_move(here("simulation", report_name), output_path)
-      # set working directory back
-      setwd(here(""))
-    }, 
-    error = function(err) {
-      #if we hit an error here, report the error in a file.log and 
-      #move on to the next simulation
-      sim_path <- here(base_folder, sim_name)
-      if (!dir_exists(sim_path)) {
-        dir_create(sim_path)
-      }
-      
-      log_file <- file.path(sim_path, "error.log")
-      
-      # Capture the error message
-      cat("Error:\n", conditionMessage(err), "\n\n", file = log_file, 
-          append = TRUE)
-      
-      # Capture the traceback
-      tb <- capture.output(traceback())
-      cat("Traceback:\n", paste(tb, collapse = "\n"), "\n\n", file = log_file, 
-          append = TRUE)
-    })
+  run_simulation(sim_name, 
+                 pop_start = pop_start,
+                 segment_df = segment_df,
+                 mar = mar,
+                 ancestry = ancestry,
+                 fert_multiplier = fert_multiplier,
+                 seed = seed)
+  
+  # Prepare diagnostic report within a try/catch
+  tryCatch({
+    # render report and then move it
+    report_name <- paste0("diagnostics_", sim_name, ".html")
+    quarto_render(input = here("simulation", "check_simulation.qmd"), 
+                  output_format = "html",
+                  output_file = report_name,
+                  execute_params = list(sim = sim_name, sheet_id = sheet_id))
+    file_move(here("simulation", report_name), here(output_path, report_name))
+    
+  }, error = function(e) {
+    sim_path <- here(base_folder, sim_name)
+    if (!dir_exists(sim_path)) {
+      dir_create(sim_path)
+    }
+    
+    log_file <- file.path(sim_path, "error_render.log")
+    cat("Render Error:\n", conditionMessage(e), "\n\n", file = log_file)
+    tb <- capture.output(traceback())
+    cat("Traceback:\n", paste(tb, collapse = "\n"), "\n\n", file = log_file, 
+        append = TRUE)
+  })
 }
